0d881b3c624feb1e9a9e6911c150ed6d80b1bd61
FreezingSnail <36997863+FreezingSnail@users.noreply.github.com>
2018-03-29 12:24:53 -0400

    Delete bossActors.h

--- bossActors.h
+++
@@ -1,9 +0,0 @@
-//#include "actor.h"
-
-//class boss : public actor{
-  
-//public:
- // boss(const actor& BOSS(char* nm, int h, int str, int def, int spd, int spc), byte BMP, equpment Item);
-  
-//};
-
